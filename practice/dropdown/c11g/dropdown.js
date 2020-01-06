0837bc8931c7b7804bc268f64ab1823428b29aec
c11g <choyoungkwang@nts-corp.com>
2020-01-07 00:09:31 +0900

    keyboard 이벤트 추가(blur, focus 시점 검토필)

--- practice/dropdown/c11g/dropdown.js
+++ practice/dropdown/c11g/dropdown.js
@@ -8,15 +8,22 @@ class Dropdown {
     
     // 선택된 옵션 index
     this.selectedIndex = 0;
+    this.keyMap = {
+      up: 38,
+      down: 40,
+      enter: 13,
+      esc: 27,
+    };
 
     // add events
     this.addEvents();
   }
 
   toggleExpanded() {
-    if ( this.button.getAttribute('aria-expanded') ) {
+    if (this.button.getAttribute('aria-expanded')) {
       this.button.removeAttribute('aria-expanded');
       this.listbox.classList.add('hidden');
+      this.button.focus();
     } else {
       this.button.setAttribute('aria-expanded', true);
       this.listbox.classList.remove('hidden');
@@ -26,8 +33,9 @@ class Dropdown {
   selectOption() {
     const currentOption = this.options[this.selectedIndex];
     
+    // 옵션을 순회하면서 기존에 선택된 옵션 해제
     this.options.some(option => {
-      if ( option.getAttribute('aria-selected') ) {
+      if (option.getAttribute('aria-selected')) {
         option.removeAttribute('aria-selected');
         option.classList.remove('focused');
         return true;
@@ -42,17 +50,43 @@ class Dropdown {
   }
 
   addEvents() {
-    this.button.addEventListener('click', e => {
+    this.button.addEventListener('click', () => {
+      console.log('button click')
       this.toggleExpanded();
-      this.listbox.focus();
       this.selectOption();
+      this.listbox.focus();
     });
 
     this.listbox.addEventListener('click', e => {
-      if ( e.target.getAttribute('aria-selected') ) return;
-      const clikedOptionIndex = this.options.indexOf(e.target);
-      this.selectedIndex = clikedOptionIndex;
+      if (e.target.getAttribute('role') !== 'option' || e.target.getAttribute('aria-selected')) return;
+      
+      this.selectedIndex = this.options.indexOf(e.target);
       this.selectOption();
+    });
+
+    this.listbox.addEventListener('keydown', e => {
+      switch ( e.keyCode ) {
+        case this.keyMap.up:
+          if (this.selectedIndex <= 0) return;
+          this.selectedIndex--;
+        break;
+        case this.keyMap.down:
+          if (this.selectedIndex >= this.options.length-1) return;
+          this.selectedIndex++;
+        break;
+        case this.keyMap.enter:
+        case this.keyMap.esc:
+          this.listbox.blur();
+          return;
+      }
+      this.selectOption();
+    });
+
+    this.listbox.addEventListener('blur', () => {
+      console.log('blur')
+      this.toggleExpanded();
     });
   }
 }
+
+export default Dropdown;
