cb89e551af7d9fbe89255c683f8acb8a22d74ae6
c11g <choyoungkwang@nts-corp.com>
2020-01-13 23:33:34 +0900

    add, remove handler 등록 및 기타
    - delete -> remove 변경(예약어 이슈)
    - delete 로직을 별도 메서드에서 handler에서 처리하는걸로 변경(너무 간단할거 같아서)

--- practice/modifyDOM/c11g/app.js
+++ practice/modifyDOM/c11g/app.js
@@ -2,7 +2,7 @@
 [ ] 추가 버튼: .card 요소 생성 -> makeCard
 [ ] .card 요소에 넘버링 추가
 [ ] .card 요소에 랜덤한 배경 컬러(#2e8b57) -> getRandomColor
-[ ] 삭제 버튼: 마지막에 위치한 .card 요소 제거 -> deleteCard
+[ ] 삭제 버튼: 마지막에 위치한 .card 요소 제거 -> removeCard
 [ ] .card의 갯수가 1개 보다 작아지는 경우는 없습니다.(alert 경고 문구 노출)
 [ ] .card 클릭: 배경 컬러 변경
 
@@ -19,7 +19,7 @@ const App = class {
     // DOM 참조
     this.el = {
       add: document.querySelector('#add'),
-      delete: document.querySelector('#delete'),
+      remove: document.querySelector('#delete'),
     };
 
     // 이벤트 등록
@@ -30,12 +30,18 @@ const App = class {
     
   }
 
-  deleteCard() {
+  addHandler() {
+    console.log('call addHandler');
+  }
 
+  removeHandler() {
+    console.log('call removeHandler');
   }
 
   addEvents() {
-    
+    const { add, remove } = this.el;
+    add.addEventListener('click', this.addHandler);
+    remove.addEventListener('click', this.removeHandler);
   }
 };
 
