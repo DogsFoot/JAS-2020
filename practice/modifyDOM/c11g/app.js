06313251ca683f3d3be05d0eab2c11330b00b4bb
c11g <choyoungkwang@nts-corp.com>
2020-01-13 23:07:15 +0900

    과제 준비

---
+++ practice/modifyDOM/c11g/app.js
@@ -0,0 +1,19 @@
+/* TODO
+[ ] 추가 버튼: .card 요소 생성
+[ ] .card 요소에 넘버링 추가
+[ ] .card 요소에 랜덤한 배경 컬러(#2e8b57)
+[ ] 삭제 버튼: 마지막에 위치한 .card 요소 제거
+[ ] .card의 갯수가 1개 보다 작아지는 경우는 없습니다.(alert 경고 문구 노출)
+[ ] .card 클릭: 배경 컬러 변경
+
+.card: <a class="card" role="button" style="background:#2e8b57">1</a>
+*/
+const App = class {
+  constructor() {
+    console.log('app fire')
+  }
+
+  
+};
+
+export default App;
