1a00cf5128fa5b156ebf4662c24903a03aa1dae5
c11g <choyoungkwang@nts-corp.com>
2020-01-16 23:59:41 +0900

    removeHandler, cardClickHandler  - getRandomHexColor 클래스 내부로 이동

--- practice/modifyDOM/c11g/app.js
+++ practice/modifyDOM/c11g/app.js
@@ -1,58 +1,58 @@
-/* TODO
-[v]] 추가 버튼: .card 요소 모델에 추가 -> makeCard
-[v] .card 요소에 넘버링 추가
-[v] .card 요소에 랜덤한 배경 컬러(#2e8b57) -> getRandomHexColor
-[ ] 삭제 버튼: 마지막에 위치한 .card 요소 제거 -> removeCard
-[ ] .card의 갯수가 1개 보다 작아지는 경우는 없습니다.(alert 경고 문구 노출)
-[ ] .card 클릭: 배경 컬러 변경
-
-.card: <a class="card" role="button" style="background:#2e8b57">1</a>
-*/
-const getRandomHexColor = () =>
-  `rgb(${Math.ceil(Math.random() * 255)},${Math.ceil(
-    Math.random() * 255
-  )},${Math.ceil(Math.random() * 255)})`;
-
 const App = class {
-  constructor() {
-    console.log("app fire");
+	constructor() {
+		// DOM 참조
+		this.el = {
+			add: document.querySelector('#add'),
+			remove: document.querySelector('#delete'),
+			wrap: document.querySelector('#card-wrap')
+		};
 
-    // DOM 참조
-    this.el = {
-      add: document.querySelector("#add"),
-      remove: document.querySelector("#delete")
-    };
+		// 이벤트 등록
+		this.addEvents();
 
-    // 이벤트 등록
-    this.addEvents();
+		// 초기화
+		this.addHandler();
+	}
 
-    // 초기화
-    this.addHandler();
-  }
+	getRandomHexColor() {
+		return `rgb(${Math.ceil(Math.random() * 255)},${Math.ceil(Math.random() * 255)},${Math.ceil(
+			Math.random() * 255
+		)})`;
+	}
 
-  makeCard() {
-    const card = document.createElement("a");
-    card.setAttribute("role", "button");
-    card.classList.add("card");
-    card.style.background = getRandomHexColor();
-    card.textContent = document.querySelectorAll(".card").length + 1;
-    return card;
-  }
+	makeCard() {
+		const { wrap } = this.el;
+		const card = document.createElement('a');
+		card.setAttribute('role', 'button');
+		card.classList.add('card');
+		card.style.background = this.getRandomHexColor();
+		card.textContent = wrap.childElementCount + 1;
+		return card;
+	}
 
-  addHandler() {
-    console.log("call addHandler");
-    document.querySelector("#card-wrap").appendChild(this.makeCard());
-  }
+	addHandler() {
+		const { wrap } = this.el;
+		wrap.appendChild(this.makeCard());
+	}
 
-  removeHandler() {
-    console.log("call removeHandler");
-  }
+	removeHandler() {
+		const { wrap } = this.el;
+		if (wrap.childElementCount <= 1) return alert('카드는 1개 이상 있어야 합니다.');
+		wrap.lastElementChild.remove();
+	}
 
-  addEvents() {
-    const { add, remove } = this.el;
-    add.addEventListener("click", () => this.addHandler());
-    remove.addEventListener("click", () => this.removeHandler());
-  }
+	cardClickHandler(e) {
+		const target = e.target;
+		if (!target.classList.contains('card')) return;
+		target.style.background = this.getRandomHexColor();
+	}
+
+	addEvents() {
+		const { add, remove, wrap } = this.el;
+		add.addEventListener('click', () => this.addHandler());
+		remove.addEventListener('click', () => this.removeHandler());
+		wrap.addEventListener('click', (e) => this.cardClickHandler(e));
+	}
 };
 
 export default App;
