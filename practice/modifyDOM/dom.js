81ba053af9978ea3b6d342e72e8c2d69ea386bd9
정경주 <kyeongjoo.jung@partner.sk.com>
2020-01-17 14:24:49 +0900

    코드리뷰 반영 리팩토링

--- practice/modifyDOM/dom.js
+++ practice/modifyDOM/dom.js
@@ -2,40 +2,49 @@
 	const cardModify = {
 		cardWrap: document.getElementById('card-wrap'),
 		add () {
-			const cardElement = `<a class="card" role="button" style="background-color:#${getRandomColor()}">${this.cardWrap.childElementCount += 1}</a>`;
+			const cardElement = `<a class="card" role="button" style="background-color: #${getRandomColor()}">${this.cardWrap.childElementCount += 1}</a>`;
 
 			this.cardWrap.innerHTML += cardElement;
 		},
 		delete () {
-			if (this.cardWrap.childElementCount > 1) {
-				this.cardWrap.removeChild(this.cardWrap.lastElementChild);
-			} else {
-				alert('카드는 최소 1개는 있어야 합니다.');
-			}
+			if (this.cardWrap.childElementCount < 2) return alert('카드는 최소 1개는 있어야 합니다.');
+			this.cardWrap.removeChild(this.cardWrap.lastElementChild);
 		}
 	};
 
 	function getRandomColor () {
-		const hexCodeArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
-		let hexCode = '';
+		const rgb = [];
 
-		for (let i = 0; i < 6; i++) {
-			hexCode += hexCodeArray[Math.floor(Math.random()*hexCodeArray.length)];
+		for (let i = 0; i < 3; i++) {
+			rgb.push(Math.floor(Math.random()*256));
 		}
+	
+		return rgb.reduce((acc, curr) => {
+			if (acc < 16 && typeof acc === 'number') {
+				acc = `0${acc.toString(16)}`;
+			} else {
+				acc = acc.toString(16);
+			}
+			if (curr < 16) {
+				curr = `0${curr.toString(16)}`;
+			} else {
+				curr = curr.toString(16);
+			}
 
-		return hexCode;
+			return acc + curr;
+		});
 	}
 
-	const adding = cardModify.add.bind(cardModify);
-	const deleting = cardModify.delete.bind(cardModify);
+	const bindAdd = cardModify.add.bind(cardModify);
+	const bindDelete = cardModify.delete.bind(cardModify);
 
-	document.getElementById('add').addEventListener('click', adding);
-	document.getElementById('delete').addEventListener('click', deleting);
+	document.getElementById('add').addEventListener('click', bindAdd);
+	document.getElementById('delete').addEventListener('click', bindDelete);
 	document.getElementById('card-wrap').addEventListener('click', function (e) {
-		const card = e.target;
+		const target = e.target;
 
-		if (card !== this) {
-			card.attributes['style'].textContent = `background-color: #${getRandomColor()}`;
+		if (target.getAttribute('role') === 'button') {
+			target.attributes['style'].textContent = `background-color: #${getRandomColor()}`;
 		}
 	});
 })();
