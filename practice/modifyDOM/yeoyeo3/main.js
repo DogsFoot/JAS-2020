eed39d3cd940fffd1bbbe985933b7a25683a3956
yeoyeo3 <beingboring.yeoyeo3@gmail.com>
2020-01-24 02:36:32 +0900

    Add files and setting jquery

---
+++ practice/modifyDOM/yeoyeo3/main.js
@@ -0,0 +1,24 @@
+let elementBtnAdd = $('#add');
+let elementBtnRemove = $('#delete');
+let elementCardWarp = $('#card-wrap');
+let elementCard = $('.card');
+
+let add = () => {
+}
+
+let remove = () => {
+}
+
+let colorMaker = () => {
+}
+
+elementBtnAdd.on('click',function(){
+});
+
+elementBtnRemove.on('click', function(){
+});
+
+elementCard.on('click', function(){
+});
+
+
