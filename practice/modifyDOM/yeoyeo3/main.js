541a405be74f79105b1b4f0efd1d86ad573a3718
yeoyeo3 <beingboring.yeoyeo3@gmail.com>
2020-01-25 16:22:55 +0900

    즉시 실행함수로 감쌈, const로 변경

--- practice/modifyDOM/yeoyeo3/main.js
+++ practice/modifyDOM/yeoyeo3/main.js
@@ -1,24 +1,25 @@
-let elementBtnAdd = $('#add');
-let elementBtnRemove = $('#delete');
-let elementCardWarp = $('#card-wrap');
+(function(){
+const elementBtnAdd = $('#add');
+const elementBtnRemove = $('#delete');
+const elementCardWarp = $('#card-wrap');
 
-let add = () => {
-	let cardLength = elementCardWarp.find('a').length;
-	let newCard = elementCardWarp.find('a:first-child').clone();
-	newCard.text(cardLength + 1).css('background', colorMaker());
+const add = () => {
+	const cardLength = elementCardWarp.find('a').length;
+	const newCard = elementCardWarp.find('a:first-child').clone();
+	newCard.text(cardLength + 1).css('background', getRandomColor());
 	elementCardWarp.append(newCard);
 }
 
-let remove = () => {
+const remove = () => {
 	elementCardWarp.find('a:last-child').remove();
 }
 
-let colorMaker = () => {
-	let colorR = Math.round(Math.random() * 0xFF).toString(16);
-	let colorG = Math.round(Math.random() * 0xFF).toString(16);
-	let colorB = Math.round(Math.random() * 0xFF).toString(16);
-	let color = '#' + colorR + colorG + colorB;
-	return color;
+const getRandomColor = () => {
+	const hexColorR = Math.round(Math.random() * 0xFF).toString(16);
+	const hexColorG = Math.round(Math.random() * 0xFF).toString(16);
+	const hexColorB = Math.round(Math.random() * 0xFF).toString(16);
+	const hexColor = '#' + hexColorR + hexColorG + hexColorB;
+	return hexColor;
 }
 
 elementBtnAdd.on('click',function(){
@@ -26,17 +27,17 @@ elementBtnAdd.on('click',function(){
 });
 
 elementBtnRemove.on('click', function(){
-	let cardLength = elementCardWarp.find('a').length;
+	const cardLength = elementCardWarp.find('a').length;
 	if (cardLength === 1) {
 		alert( '경고 : 제거할 수 없습니다');
 	} else remove();
 });
 
 elementCardWarp.on('click', function(e){
-	let thisCard = $(e.target);
+	const thisCard = $(e.target);
 	if(thisCard.hasClass('card')) {
-		thisCard.css('background', colorMaker());
+		thisCard.css('background', getRandomColor());
 	}
 });
-
+})();
 
