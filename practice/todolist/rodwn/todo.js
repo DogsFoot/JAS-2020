f49d3630ab9ed57efd4fd4e516c64fff5520c0f6
정경주 <kyeongjoo.jung@partner.sk.com>
2020-01-31 14:17:53 +0900

    2차기능 달성

--- practice/todolist/rodwn/todo.js
+++ practice/todolist/rodwn/todo.js
@@ -1,49 +1,69 @@
 const ToDo = {
 	toDoRapper: document.querySelectorAll('.todo-wrap'),
 
-	init () {
-		this.toDoRapper.forEach(eachToDo => this.addToDoEvent(eachToDo));
+	addTodoEvent () {
+		this.toDoRapper.forEach((eachToDo, index) => this.ToDoEvent(eachToDo, index));
 	},
 
-	addToDoEvent (eachToDoWrapper) {
+	ToDoEvent (eachToDoWrapper, index) {
 		const input = eachToDoWrapper.querySelector('.todo-input'),
-			addButton = eachToDoWrapper.querySelector('.todo-add');
+			addButton = eachToDoWrapper.querySelector('.todo-add'),
+			list =  eachToDoWrapper.querySelector('.todo-list');
 		
-
+		// 로컬스토리지에서 데이터 불러온후 li 뿌리기
+		list.innerHTML = localStorage.getItem(eachToDoWrapper.className+(index+1));
+		
+		// 뿌려진 li내에 이벤트 걸기
+		[...list.children].forEach(li => li.addEventListener('click', addEventForListItem));
+		
+		// li추가 이벤트 걸기
 		addButton.addEventListener('click', addListItem);
 
+		// li추가 이벤트
 		function addListItem () {
-			const list = eachToDoWrapper.querySelector('.todo-list')
-				elementLI = document.createElement('li');
+			const elementLI = document.createElement('li');
 
 			if (input.value.trim().length <= 0) {
 				return alert('입력해주세요.');
 			}
-			elementLI.setAttribute('id', list.childElementCount+=1);
+
+			elementLI.setAttribute('id', (list.childElementCount) ? Number(list.lastElementChild.id) + 1 : 1);
 			elementLI.innerHTML = `<input type="checkbox" class="todo-checkbox"> <span>${input.value}</span> <button type="button" class="todo-delete">삭제</button>`;
-			elementLI.addEventListener('click', function (e) {
-				const target = e.target;
-
-				if (target.getAttribute('type') === 'checkbox') {
-					if (target.classList.contains('done')) {
-						target.classList.remove('done');
-						target.removeAttribute('checked');
-					} else {
-						target.classList.add('done');
-						target.setAttribute('checked','');
-					}
-				}
-
-				if (target.getAttribute('class') === 'todo-delete') {
-					list.removeChild(this);
-				}
-			});
+			elementLI.addEventListener('click', addEventForListItem);
 
 			input.value = '';
 			input.focus();
 			list.appendChild(elementLI);
+			localStorage.setItem(eachToDoWrapper.className+(index+1), list.innerHTML);
 		}
+
+		// li내 체크및삭제 이벤트
+		function addEventForListItem (e) {
+			const target = e.target;
+
+			if (target.getAttribute('type') === 'checkbox') {
+				if (target.classList.contains('done')) {
+					target.classList.remove('done');
+					target.removeAttribute('checked');
+				} else {
+					target.classList.add('done');
+					target.setAttribute('checked', '');
+				}
+			}
+
+			if (target.getAttribute('class') === 'todo-delete') {
+				list.removeChild(this);
+			}
+			localStorage.setItem(eachToDoWrapper.className+(index+1), list.innerHTML);
+		}
+	},
+
+	init () {
+		localStorage.clear();
+		this.toDoRapper.forEach((eachToDo) => {
+			eachToDo.querySelector('.todo-list').innerHTML = '';
+		});
 	}
 }
 
-ToDo.init();
+ToDo.addTodoEvent();
