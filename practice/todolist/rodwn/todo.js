a8931ca35d940f3aa85bd00b8746467963eb1b1b
정경주 <kyeongjoo.jung@partner.sk.com>
2020-02-03 17:52:32 +0900

    코드리뷰 반영 1차 기능 달성

--- practice/todolist/rodwn/todo.js
+++ practice/todolist/rodwn/todo.js
@@ -1,69 +1,62 @@
 const ToDo = {
-	toDoRapper: document.querySelectorAll('.todo-wrap'),
-
-	addTodoEvent () {
-		this.toDoRapper.forEach((eachToDo, index) => this.ToDoEvent(eachToDo, index));
-	},
-
-	ToDoEvent (eachToDoWrapper, index) {
-		const input = eachToDoWrapper.querySelector('.todo-input'),
-			addButton = eachToDoWrapper.querySelector('.todo-add'),
-			list =  eachToDoWrapper.querySelector('.todo-list');
-		
-		// 로컬스토리지에서 데이터 불러온후 li 뿌리기
-		list.innerHTML = localStorage.getItem(eachToDoWrapper.className+(index+1));
-		
-		// 뿌려진 li내에 이벤트 걸기
-		[...list.children].forEach(li => li.addEventListener('click', addEventForListItem));
-		
-		// li추가 이벤트 걸기
-		addButton.addEventListener('click', addListItem);
-
-		// li추가 이벤트
-		function addListItem () {
-			const elementLI = document.createElement('li');
-
-			if (input.value.trim().length <= 0) {
-				return alert('입력해주세요.');
-			}
-
-			elementLI.setAttribute('id', (list.childElementCount) ? Number(list.lastElementChild.id) + 1 : 1);
-			elementLI.innerHTML = `<input type="checkbox" class="todo-checkbox"> <span>${input.value}</span> <button type="button" class="todo-delete">삭제</button>`;
-			elementLI.addEventListener('click', addEventForListItem);
-
-			input.value = '';
-			input.focus();
-			list.appendChild(elementLI);
-			localStorage.setItem(eachToDoWrapper.className+(index+1), list.innerHTML);
-		}
-
-		// li내 체크및삭제 이벤트
-		function addEventForListItem (e) {
-			const target = e.target;
-
-			if (target.getAttribute('type') === 'checkbox') {
-				if (target.classList.contains('done')) {
-					target.classList.remove('done');
-					target.removeAttribute('checked');
-				} else {
-					target.classList.add('done');
-					target.setAttribute('checked', '');
-				}
-			}
-
-			if (target.getAttribute('class') === 'todo-delete') {
-				list.removeChild(this);
-			}
-			localStorage.setItem(eachToDoWrapper.className+(index+1), list.innerHTML);
-		}
-	},
+	toDoWrapper: document.querySelector('.todo-wrap'),
+	resetButton: document.querySelector('.btn-reset'),
+	addButton: document.querySelector('.todo-wrap .todo-add'),
+	input: document.querySelector('.todo-wrap .todo-input'),
+	list: document.querySelector('.todo-wrap .todo-list'),
 
 	init () {
+		const bindAddListItem = this.addListItem.bind(ToDo),
+			bindResetList = this.resetList.bind(ToDo);
+
+		this.resetButton.addEventListener('click', bindResetList);
+		this.addButton.addEventListener('click', bindAddListItem);
+		this.list.addEventListener('click', this.addCheckboxToggle);
+		this.list.addEventListener('click', this.removeListItem);
+	},
+
+	addListItem () {
+		const elementLI = document.createElement('li');
+		
+		if (this.input.value.trim().length <= 0) {
+			return alert('입력해주세요.');
+		}
+
+		elementLI.setAttribute('id', (this.list.childElementCount) ? Number(this.list.lastElementChild.id) + 1 : 1);
+		elementLI.innerHTML = `<input type="checkbox" class="todo-checkbox"> <span>${this.input.value}</span> <button type="button" class="todo-delete">삭제</button>`;
+
+		this.input.value = '';
+		this.input.focus();
+		this.list.appendChild(elementLI);
+
+	},
+	
+	addCheckboxToggle: (e) => {
+		const target = e.target;
+
+		if (target.classList.contains('todo-checkbox')) {
+			if (target.classList.contains('done') && target.getAttribute('checked') === 'checked') {
+				target.classList.remove('done');
+				target.removeAttribute('checked');
+			} else {
+				target.classList.add('done');
+				target.setAttribute('checked', 'checked')
+			}
+		}
+	},
+
+	removeListItem: (e) => {
+		const target = e.target; 
+
+		if (target.classList.contains('todo-delete')) {
+			ToDo.list.removeChild(target.parentElement);
+		}
+	},
+
+	resetList () {
 		localStorage.clear();
-		this.toDoRapper.forEach((eachToDo) => {
-			eachToDo.querySelector('.todo-list').innerHTML = '';
-		});
+		this.list.innerHTML = '';
 	}
 }
 
-ToDo.addTodoEvent();
+ToDo.init();
