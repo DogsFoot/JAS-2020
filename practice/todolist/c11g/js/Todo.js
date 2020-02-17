dfc76af5836606038486b0644fa9dd1b717186f3
c11g <renimo@hanmail.net>
2020-02-17 14:08:46 +0900

    localstorage 기능 추가

--- practice/todolist/c11g/js/Todo.js
+++ practice/todolist/c11g/js/Todo.js
@@ -1,6 +1,9 @@
+import Task from './Task.js';
+
 const Todo = class {
   constructor() {
-    this.tasks = new Set();
+    this.STORAGE_KEY = 'tasks';
+    this.tasks = this.load() || new Set();
   }
 
   addTask(task) {
@@ -9,6 +12,21 @@ const Todo = class {
 
   removeTask(task) {
     this.tasks.delete(task);
+  }
+
+  save() {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify([...this.tasks]));
+  }
+
+  load() {
+    if (!localStorage.getItem(this.STORAGE_KEY)) return null;
+    const data = JSON.parse(localStorage.getItem(this.STORAGE_KEY));
+    const savedTask = data.reduce((tasks, t) => {
+      const task = new Task(t.title, t.id, t.done);
+      tasks.add(task);
+      return tasks;
+    }, new Set());
+    return savedTask;
   }
 }
 
