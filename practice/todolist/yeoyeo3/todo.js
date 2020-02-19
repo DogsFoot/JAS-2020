9fa1372394e6b8cc2094ec5800b6fab470f9ca24
yeoyeo3 <beingboring.yeoyeo3@gmail.com>
2020-02-20 01:04:02 +0900

    리무브구현중, done된 todo최초 뿌려줄시 체크된채로 뿌려주기

--- practice/todolist/yeoyeo3/todo.js
+++ practice/todolist/yeoyeo3/todo.js
@@ -4,13 +4,43 @@ class Todo {
     this.elementInput = $wrap.find('.todo-input');
     this.elementBtnAdd = $wrap.find('.todo-add');
     this.elementListTodo = $wrap.find('.todo-list');
-    this.dataTodo = [];
+    this.localStorageKey = this.elementTodo.attr('id');
+    this.init();
   }
 
   init(){
+    const inintData = [];
+    if(!localStorage.getItem(this.localStorageKey)){
+      localStorage.setItem(this.localStorageKey, JSON.stringify(inintData));
+    } else {
+      let allTodoDatas = this.storageData('get');
+      allTodoDatas.forEach(todo => {
+        const newTodoItem = $('<li>');
+        newTodoItem.attr('id', todo.id)
+          .append($('<input>').attr('type','checkbox').addClass('todo-checkbox'))
+          .append($('<span>').text(todo.content))
+          .append($('<button>').attr('type','button').addClass('todo-delete').text('삭제'));
+        this.elementListTodo.append(newTodoItem);
+      });
+    }
 		this.addHandler();
 		this.removeHandler();
 		this.doneHandler(); 
+  }
+
+  storageData(action, data) {
+    let allTodoDatas = localStorage.getItem(this.localStorageKey);
+    allTodoDatas = JSON.parse(allTodoDatas);
+    if(action == 'get'){
+      return allTodoDatas;
+    }
+    if(action == 'add'){
+      allTodoDatas.push(data);
+    }
+    if(action == 'doneModify'){
+      allTodoDatas[data].done = !allTodoDatas[data].done;
+    }
+    localStorage.setItem(this.localStorageKey, JSON.stringify(allTodoDatas));
   }
 
   addHandler(){
@@ -48,18 +78,19 @@ class Todo {
 
   addTodo(){
     const content = this.elementInput.val();
+    const CreatTodoData = function(content){
+      this.id = new Date().getTime();
+      this.content = content;
+      this.done = false;
+    };
 
     if(content === '') {
       alert('입력해주세요');
     } else {
-      const newTodoData = {};
-      const newTodoItem = $('<li>')
-      
-      newTodoData.status = 'todo';
-      newTodoData.content = content;
-      this.dataTodo.push(newTodoData);
-      
-      newTodoItem.attr('id', this.dataTodo.length)
+      const newTodoData = new CreatTodoData(content);
+      this.storageData('add', newTodoData);
+      const newTodoItem = $('<li>');
+      newTodoItem.attr('id', newTodoData.id)
         .append($('<input>').attr('type','checkbox').addClass('todo-checkbox'))
         .append($('<span>').text(content))
         .append($('<button>').attr('type','button').addClass('todo-delete').text('삭제'));
@@ -71,24 +102,33 @@ class Todo {
 
   doneTodo(thisElementTodoItem){
     const toDoneItemIndex = thisElementTodoItem.attr('id');
+    const allTodoDatas = this.storageData('get');
+    let todoDataIndex = '';
+    allTodoDatas.some( (todo, index) => {
+      if( toDoneItemIndex == todo.id ) {
+        todoDataIndex = index;
+      }
+    });
 
-    if(thisElementTodoItem.find('input').hasClass('done')) {
-      this.dataTodo[toDoneItemIndex - 1].status = 'todo';
+    if(allTodoDatas[todoDataIndex].done === true){
       this.elementListTodo.find('#' + toDoneItemIndex).find('input')
         .attr('checked', false)
         .removeClass('done');
     } else {
-      this.dataTodo[toDoneItemIndex - 1].status = 'done';
       this.elementListTodo.find('#' + toDoneItemIndex).find('input')
         .attr('checked', true)
         .addClass('done');
     }
+
+    this.storageData('doneModify', todoDataIndex);
   }
 
   removeTodo(thisElementTodoItem){
     const toRemoveItemIndex = thisElementTodoItem.attr('id');
 
-    this.dataTodo.splice(toRemoveItemIndex - 1, 1);
+
+    this.storageData('remove', newTodoData);
+    localStorage.removeItem(toRemoveItemIndex);
     this.elementListTodo.find('#' + toRemoveItemIndex).remove();
   }
 }
