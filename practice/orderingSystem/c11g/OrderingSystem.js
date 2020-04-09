465c199dd942ee4f43170650c1583c6db3e51ed3
c11g <renimo@hanmail.net>
2020-04-10 00:31:15 +0900

    _ 기능 구현 완료 및 코드 정리 전

--- practice/orderingSystem/c11g/OrderingSystem.js
+++ practice/orderingSystem/c11g/OrderingSystem.js
@@ -6,22 +6,26 @@ class OrderingSystem {
     this.cart = {};
     this.shopOrderList = [];
     this.customerOrderList = [];
+    this.statusMsg = ['발주 신청','발주 완료','발주 불가'];
+    document.querySelector('#test').addEventListener('click', _ => console.log(this.products)); // TEST
     
     this.el = {
       shopProdcuts: document.querySelector('.shop .product'),
       customerProdcuts: document.querySelector('.customer .product'),
       cart: document.querySelector('.cart'),
-      orderButton: document.querySelector('.btn-submit'),
+      shopOrder: document.querySelector('.shop .order.list'),
+      customerOrder: document.querySelector('.customer .order.list'),
+      placeOrder: document.querySelector('.btn-submit'),
       shopOrder: document.querySelector('.shop .order'),
       customerOrder: document.querySelector('.customer .order'),
     };
 
-    this.renderProducts(inventory);
+    this.renderProducts();
     this.addEvents();
   }
 
   getProducts(inventory){
-    return inventory.map(({id, name, stock, imgUrl}) => new Product(id, name, stock, imgUrl));
+    return inventory.map(({id,name,stock,available,imgUrl}) => new Product(id,name,stock,available,imgUrl));
   }
 
   updateCart(product, count){
@@ -31,23 +35,28 @@ class OrderingSystem {
 
   // Event & Handler
   addEvents(){
-    const {shopProdcuts,customerProdcuts,cart,orderButton} = this.el;
+    const {shopProdcuts,customerProdcuts,cart,placeOrder,shopOrder,customerOrder} = this.el;
     shopProdcuts.addEventListener('change', e => this.shopSpinnerHandler(e));
     customerProdcuts.addEventListener('change', e => this.customerSpinnerHandler(e));
     cart.addEventListener('click', e => this.deleteHandler(e));
-    orderButton.addEventListener('click', _ => this.orderHandler());
+    placeOrder.addEventListener('click', _ => this.placeOrderHandler());
+    shopOrder.addEventListener('click', e => this.shopOrderHandler(e));
+    customerOrder.addEventListener('click', e => this.cancelOrderHandler(e));
   }
 
   shopSpinnerHandler(e){
     const spinner = e.target;
-    const stock = parseInt(spinner.value);
-    if(stock < 0) {
+    const count = parseInt(spinner.value);
+    if(count < 0) {
       spinner.value = 0;
       return alert('재고를 0개 이상으로 설정해주세요.');
     }
     const product_id = spinner.parentElement.dataset.id;
     const product = this.products.find(({id}) => id === product_id);
-    product.setStock(stock);
+    const diff = count - product.stock;
+    const available = product.available;
+    product.setStock(count);
+    product.setAvailable(available+diff);
     this.renderCustomerProducts();
   }
 
@@ -59,9 +68,9 @@ class OrderingSystem {
     if(count < 0) {
       spinner.value = 0;
       return alert('주문 수량은 0개 이상으로 설정해주세요.');
-    } else if(count > product.stock) {
-      spinner.value = product.stock;
-      return alert(`재고가 부족합니다. (최대 주문가능 수량: ${product.stock}개)`);
+    } else if(count > product.available) {
+      spinner.value = product.available;
+      return alert(`재고가 부족합니다. (최대 주문가능 수량: ${product.available}개)`);
     }
     this.updateCart(product, count);
     this.renderCart();
@@ -76,13 +85,17 @@ class OrderingSystem {
     document.querySelector(`.customer [data-id=${product_id}] .spinner`).value = 0;
   }
 
-  orderHandler(){
+  placeOrderHandler(){
     if (Object.keys(this.cart).length === 0) return;
     const order = {
       date: Date.now(),
-      products: Object.values(this.cart),
-      isDone: false,
+      products: Object.entries(this.cart),
+      status: 0, // 0: 발주 신청, 1: 발주 완료, 2: 발주 불가
     }
+    order.products.forEach(product => {
+      const p = this.products.find(p => p.id === product[0]);
+      p.setAvailable(p.available-product[1][1]);
+    });
     this.shopOrderList.push(order);
     this.customerOrderList.push(order);
     this.renderShopOrder();
@@ -92,17 +105,59 @@ class OrderingSystem {
     this.renderCustomerProducts();
   }
 
+  cancelOrderHandler(e){
+    const target = e.target;
+    if (target.nodeName !== 'BUTTON') return;
+    const currentOrder = this.customerOrderList.find(order => order.date === parseInt(target.dataset.id));
+    this.customerOrderList = this.customerOrderList.filter(order => order !== currentOrder);
+    this.shopOrderList = this.shopOrderList.filter(order => order !== currentOrder);
+    currentOrder.products.forEach(product => {
+      const p = this.products.find(p => p.id === product[0]);
+      p.setAvailable(p.available+product[1][1]);
+    });
+    this.renderShopOrder();
+    this.renderCustomerOrder();
+  }
+
+  shopOrderHandler(e){
+    const target = e.target;
+    if (target.nodeName !== 'BUTTON') return;
+    const role = target.dataset.role;
+    const currentOrder = this.customerOrderList.find(order => order.date === parseInt(target.dataset.id));
+    switch(role){
+      case 'confirm':
+        currentOrder.status = 1;
+        currentOrder.products.forEach(product => {
+          const p = this.products.find(p => p.id === product[0]);
+          p.setStock(p.stock-product[1][1]);
+        });
+        this.renderProducts();
+      break;
+      case 'reject':
+        currentOrder.status = 2;
+        currentOrder.products.forEach(product => {
+          const p = this.products.find(p => p.id === product[0]);
+          p.setAvailable(p.available+product[1][1]);
+        });
+      break;
+      default: return;
+    }
+    this.shopOrderList = this.shopOrderList.filter(order => order !== currentOrder);
+    this.renderShopOrder();
+    this.renderCustomerOrder();
+  }
+
   // Render
   renderCustomerOrder(){
-    this.el.customerOrder.innerHTML = this.customerOrderList.reduce((acc, {date,products,isDone}) => `${acc}<li><span class="text-date">${date}</span><ul class="order-item">${
-      products.map(p => `<li>${p[0]} <span>${p[1]}</span></li>`).join(' ')
-    }</ul><div class="btn-area"><button type="button" class="btn-cancel" ${isDone?'disabled':''}>취소</button></div><div class="status">${isDone?'발주 완료':'발주 중'}</div></li>`,'');
+    this.el.customerOrder.innerHTML = this.customerOrderList.reduce((acc, {date,products,status}) => `${acc}<li><span class="text-date">${date}</span><ul class="order-item">${
+      products.map(p => `<li>${p[1][0]} <span>${p[1][1]}</span></li>`).join(' ')
+    }</ul><div class="btn-area"><button type="button" class="btn-cancel" ${status !== 0?'disabled':''} data-id="${date}">취소</button></div><div class="status">${this.statusMsg[status]}</div></li>`,'');
   }
 
   renderShopOrder(){
     this.el.shopOrder.innerHTML = this.shopOrderList.reduce((acc, {date,products}) => `${acc}<li><span class="text-date">${date}</span><ul class="order-item">${
-      products.map(p => `<li>${p[0]} <span>${p[1]}</span></li>`).join(' ')
-    }</ul><div class="btn-area"><button type="button" class="btn-confirm">수락</button><button type="button" class="btn-reject">불가</button></div></li>`,'');
+      products.map(p => `<li>${p[1][0]} <span>${p[1][1]}</span></li>`).join(' ')
+    }</ul><div class="btn-area"><button type="button" class="btn-confirm" data-role="confirm" data-id="${date}">수락</button><button type="button" class="btn-reject" data-role="reject" data-id="${date}">불가</button></div></li>`,'');
   }
   
   renderCart(){
@@ -111,13 +166,13 @@ class OrderingSystem {
     , '');
   }
 
-  renderProducts(inventory){
-    this.renderShopProducts(inventory);
+  renderProducts(){
+    this.renderShopProducts();
     this.renderCustomerProducts();
   }
 
-  renderShopProducts(inventory){
-    this.el.shopProdcuts.innerHTML = inventory.reduce((acc, {id,name,stock,imgUrl}) => {
+  renderShopProducts(){
+    this.el.shopProdcuts.innerHTML = this.products.reduce((acc, {id,name,stock,imgUrl}) => {
       return `${acc}
       <li class="list-item" data-id="${id}">
         <label>
@@ -133,10 +188,7 @@ class OrderingSystem {
     this.el.customerProdcuts.innerHTML = this.products.reduce((acc, {id,name,stock,imgUrl}) => {
       return `${acc}
       <li class="list-item ${stock < 1 ? 'is-disabled' : ''}" data-id="${id}">
-        <label for="for-chklist-${id}">
-        <input type="checkbox" name="order-product" id="for-chklist-${id}">
-          <span>${name}</span>
-        </label>
+        <span>${name}</span>
         <span class="thum"><img src="${imgUrl}" alt="${name}"></span>
         <input type="number" value="0" class="spinner">
       </li>`
@@ -156,19 +208,24 @@ export default OrderingSystem;
 // [v] 수량을 1이라도 올리면, 바로바로 "소매 발주신청"란에 추가됩니다
 // [v] "소매 발주신청"란에서 반대로 아이템을 제거할 수 있습니다
 // [v] 주문하기를 누르는 순간의 시간으로 "소매 발주 신청"이 텅 비게 되며, 히스토리로 옮겨집니다 `상태 : 발주 신청`
-// [ ] 신청한 발주를 도매사가 처리 결정전, 취소 할 수 있습니다
-// [ ] 도매사가 수락하면 취소할 수 없습니다
+// [v] 신청한 발주를 도매사가 처리 결정전, 취소 할 수 있습니다
+// [v] 도매사가 수락하면 취소할 수 없습니다
 // [v] is-disabled 재고가 없으면
 
 // ## 도매사 스펙 정의
 // [v] 수시로 재고수량을 조절 할 수 있습니다.
 // [v] 소매에서 발주 신청시, 다음 소매주문에서 `주문가능 수량`에서 차감됩니다
-// [ ] 주문란
-//   [ ] 수락이나 불가를 누르면 주문란 제거
-//   [ ] 주문란은 리스트형태로 쌓일 수 있습니다
-// [ ] 주문의 수락을 눌르면
-//   [ ] `재고`수량 변경
-//   [ ] 도매사에서 수락을 하면 히스토리의 상태변경 `상태 : 발주 완료`
-// [ ] 주문의 불가를 누르면
-//   [ ] `주문가능 수량` 변경
-//   [ ] 도매사에서 불가을 하면 히스토리의 상태변경 `상태 : 발주 불가`
+// [v] 주문란
+//   [v] 수락이나 불가를 누르면 주문란 제거
+//   [v] 주문란은 리스트형태로 쌓일 수 있습니다
+// [v] 주문의 수락을 눌르면
+//   [v] `재고`수량 변경
+//   [v] 도매사에서 수락을 하면 히스토리의 상태변경 `상태 : 발주 완료`
+// [v] 주문의 불가를 누르면
+//   [v] `주문가능 수량` 변경
+//   [v] 도매사에서 불가을 하면 히스토리의 상태변경 `상태 : 발주 불가`
+
+// TODO
+// [ ] 코드 중복 부분 함수로 빼기 
+// [ ] 기능 테스트
+// [ ] 디버깅용 테스트 코드 삭제
--- practice/orderingSystem/c11g/Product.js
+++ practice/orderingSystem/c11g/Product.js
@@ -1,13 +1,18 @@
 class Product {
-  constructor(id,name,stock,imgUrl){
+  constructor(id,name,stock,available,imgUrl){
     this.id = id;
     this.name = name;
     this.stock = stock;
+    this.available = available;
     this.imgUrl = imgUrl;
   }
 
   setStock(count){
     this.stock = count;
+  }
+
+  setAvailable(count){
+    this.available = count;
   }
 }
 
